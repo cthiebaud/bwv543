2c1235d4fb8b15696b53f328903115baa8ce4edd
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2022-04-09 12:34:39 +0200

    all parts generated

---
+++ export_to_musicXML.sh
@@ -0,0 +1,5 @@
+ly "musicxml" -o 1_voice_soprano.xml bwv543_fuga_soprano_alto_tenor_bass.ly
+ly "musicxml" -o 2_voice_alto.xml bwv543_fuga_soprano_alto_tenor_bass.ly
+ly "musicxml" -o 3_voice_tenor.xml bwv543_fuga_soprano_alto_tenor_bass.ly
+ly "musicxml" -o 4_voice_bass.xml bwv543_fuga_soprano_alto_tenor_bass.ly
+ly "musicxml" -o bwv543_fuga_soprano_alto_tenor_bass.xml bwv543_fuga_soprano_alto_tenor_bass.ly
